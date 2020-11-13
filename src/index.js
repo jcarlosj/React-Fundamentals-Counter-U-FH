2412527aa0a2424ac9cf690d361e878a009312dc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-13 14:48:54 -0500

    [+] JavaScript: Variables/Constantes y su alcance

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log( 'Hola desde el index.js' );
+//  Variables y constantes
+const 
+    firstName = 'Juan',
+    lastName = 'Jiménez';
+
+let number = 42;
+
+console.log( firstName, lastName, number );
+
+if( true ) {
+    const firstName = 'Carlos';
+    let number = 9;
+
+    console.log( 'if:', firstName, lastName, number );    //  Scope: solo involucra valores dentro del condicional
+}
+
+console.log( firstName, lastName, number );
