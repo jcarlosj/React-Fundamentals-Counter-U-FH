d9af764eecd4eed81027cb7ceeebfaf656c6f294
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-13 18:40:01 -0500

    [+] JavaScript: Funcion asignada a una constante (Definicion recomendada)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-//  Funciones
-function saludo( nombre ) {
+//  Funcion asignada a una constante
+const saludar = function ( nombre ) {
     return `Primer saludo para: ${ nombre }`;
 }
 
 saludo = 9;                 //  Asigna el valor a una variable que se llama como la funcion
-console .log( saludo );     //  Imprime 9 y genera un Warning. Si no asignaramos en la linea anterior imprime la referencia de la funcion no seria invocada
+console .log( saludo );     //  Genera un Error y muestra un Warning. Si no asignaramos en la linea anterior imprime la referencia de la funcion no seria invocada
 
