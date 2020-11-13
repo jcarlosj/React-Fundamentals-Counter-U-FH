5ef0dc35b3c325a26d3ae8586e3584c9a71c4e02
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-13 18:01:44 -0500

    [+] JavaScript: Objetos Literales (Mutables e Inmutables)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-//  Variables y constantes
+//  Objetos Literales
+const supersona = {
+    nombreCompleto: {
+        original: 'Kal-El',
+        terrestre: 'Clark Joseph Kent'
+    },
+    raza: 'Kriptoniano',
+    ocupaciones: [ 'Superhéroe', 'Reportero', 'Granjero' ],
+    padres: {
+        kriptonianos: {
+            padre: 'Jor-El',
+            madre: 'Lara Lor-Van'
+        },
+        terricolas: {
+            padre: 'Jonathan Kent',
+            madre: 'Martha Kent'
+        }
+    },
+    familiares: {
+        kriptonianos: {
+            tio: 'Zor-El',
+            tia: 'Alura In-Ze',
+            prima: 'Kara Zor-El',
+            clone: {
+                genetico: 'Kon-El',
+                cibernetico: 'Bizarro'
+            }
+        }
+    }
+};
+
+// console.table( supersona );
+console.log( { supersona } );
+
 const 
-    firstName = 'Juan',
-    lastName = 'Jiménez';
+    persona = supersona,                //  Asignacion por referencia (Son el mismo objeto, es decir hay mutacion)
+    supersonita = { ...supersona };     //  Clona el objeto usando el Spread (Ahora son dos objetos diferentes, es decir no hay mutacion)
 
-console.log( firstName + ' ' + lastName );
-console.log( `${ firstName } ${ lastName }` );      //  Template String usando Backtick/Backquote
 
-const fullName = `      //  Template String: Permite hacer saltos de linea y generar espacios dentro del contenido de una variable
-    ${ firstName } 
-    ${ lastName }
-    ${ 6 * 7 }
-`;
-console.log( fullName );
+persona.raza = 'terricola';             //  Muta el objeto literal original supersona
+supersonita.raza = 'valluno';           //  Un objeto nuevo no muta
 
-//  Function
-function getFullName( fullName ) {
-    return `Hola, ${ fullName }.`;      //  Template String
-}
-
-console.log( `${ getFullName( `${ firstName } ${ lastName }` ) } ¿Cómo estas?` );   //  Template String
+console.log( persona );         //  persona es el mismo objeto de supersonita
+console.log( supersonita );     //  supersonita es un objeto nuevo
