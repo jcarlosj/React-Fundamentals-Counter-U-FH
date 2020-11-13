a0b206ca27de5a1ffe8c56d191ea0a286ab4dbb9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-13 18:44:02 -0500

    [+] JavaScript: Funciones Tradicionales Vs Funciones Flecha

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-//  Funcion asignada a una constante
-const saludar = function ( nombre ) {
+//  Funcion tradicional asignada a una constante
+const saludar1 = function ( nombre ) {
     return `Primer saludo para: ${ nombre }`;
 }
 
-saludo = 9;                 //  Asigna el valor a una variable que se llama como la funcion
-console .log( saludo );     //  Genera un Error y muestra un Warning. Si no asignaramos en la linea anterior imprime la referencia de la funcion no seria invocada
+//  Funcion flecha asignada a una constante
+const saludar2 = ( nombre ) => {
+    return `Segundo saludo para: ${ nombre }`;
+}
+
+//  En ambos casos se debe invocar posterior a la inicializacion, diferente a lo que se hace con una funcion sin asignacion a una constante
+console.log( saludar1( 'Eva Sofia' ) );
+console.log( saludar2( 'Juan Carlos' ) );     
 
