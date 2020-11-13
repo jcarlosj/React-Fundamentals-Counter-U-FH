817733680caca1f5e77ba41e3876fc7643d2a5e6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-13 18:37:40 -0500

    [+] JavaScript: Funciones (Definicion no recomendada)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-//  Arreglos
-const frutas = new Array( 10 );     //  Asigna longitud por defecto para un Array
-frutas.push( 'Mango' );             //  Agrega un elemento modificando la longitud inicial (evitar insertar elementos usando push)
+//  Funciones
+function saludo( nombre ) {
+    return `Primer saludo para: ${ nombre }`;
+}
 
-console.log( frutas );
+saludo = 9;                 //  Asigna el valor a una variable que se llama como la funcion
+console .log( saludo );     //  Imprime 9 y genera un Warning. Si no asignaramos en la linea anterior imprime la referencia de la funcion no seria invocada
 
-const 
-    numeros = [ 1, 2, 3, 4 ],
-    numeros2 = numeros,                //  Asignacion por referencia. Son el mismo objeto (muneros y numeros2 ), es decir hay mutacion
-    numeros3 = [ ...numeros, 5 ];      //  Clona el objeto usando el Spread e inserta nuevo elemento. Ahora son dos objetos diferentes ( numeros y numeros3 ) es decir no hay mutacion)
-
-console.log( numeros2 );     //  numeros2 es el mismo objeto de numeros
-console.log( numeros3 );     //  numeros3 es un objeto nuevo 
-
-/** Crear un nuevo Array usando la funcion map() */
-const numeros4 = numeros3.map( function( numero ) {
-    return numero * 3;          //  Si no se asigna return devolvera undefined por cada posicion del Array
-});
-
-console.log( numeros4 );        //  Se muestra un Array nuevo a partir del mapeo de numeros3 
