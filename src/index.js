7d5471136168b39efd73bac1e09d0f2b9588c212
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 15:59:18 -0500

    [+] JavaScript: Async/Await manejando errores. Funcion para desplegar gif aleatoriamente

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ const
     URL = `http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`;
 
 const getImage = async () => {                  
-    const 
-        response = await fetch( URL ),          //  -> Retorna una Promesa
-        { data } = await response.json(),       //  -> Retorna una Promesa
-        { original: { url } } = data.images;
-
-    console.log( url );
-    if( ! url ) return;
     
-    const $img = document.createElement( 'img' );
+    try {
+        const 
+            response = await fetch( URL ),          //  -> Retorna una Promesa
+            { data } = await response.json(),       //  -> Retorna una Promesa
+            { original: { url } } = data.images;
 
-    $img.src = url;
-    document.body.append( $img );
+        console.log( url );
+        
+        const $img = document.createElement( 'img' );
+
+        $img.src = url;
+        document.body.append( $img );
+
+    } 
+    catch( error ) {
+        console.error( error );
+        //  Manejar el error con una imagen por defecto
+    }
 }    
 
 getImage();
