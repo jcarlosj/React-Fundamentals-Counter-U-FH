36b645c70efd3e8842a88cb3636ed487b2a05a1a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 14:22:29 -0500

    [+] JavaScript: Promises (resolve/reject) & (then/catch)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
+//  Imports & Exports
+import { getHeroById } from './functions';
+
 //  Promises (Promesas)
 const promesa = new Promise( ( resolve, reject ) => {
     const seg = 2;
 
     setTimeout( () => {
-        resolve( seg + ' segundos despues' );
+        const hero = getHeroById( 2 );
+        
+        if( ! hero ) {
+            reject( 'No se encontró el héroe' );
+        } 
+
+        resolve( hero );
+        
     }, seg * 1000 );
 
 });
 
 promesa 
     .then( data => {
-        console .log( data );
+        console.log( data );
     })
-    .catch()
+    .catch( err => {
+        console.warn( err );
+    })
     .finally();
