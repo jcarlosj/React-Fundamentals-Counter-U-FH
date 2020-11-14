f3a9606de30eff2d80cde2ba4ff99a249df1d7c8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 15:20:13 -0500

    [+] JavaScript: Fetch API con promesas en cadena, despliega gif aleatoriamente

--- src/index.js
+++ src/index.js
@@ -7,7 +7,18 @@ const httpRequest = fetch( url );
 
 httpRequest                                 //  -> Retorna una Promesa
     .then( response => response.json() )    //  -> Retorna una Promesa
-    .then( data => {
-        console.log( data );                //  -> Retorna los datos
+    .then( ({ data }) => {
+        const { original: { url } } = data.images;                //  -> Retorna los datos
+
+        console.log( url );
+        if( ! url ) return 'No hay URL';
+        
+        return url;
+    })
+    .then( url => {
+        const $img = document.createElement( 'img' );
+
+        $img.src = url;
+        document.body.append( $img );
     })
     .catch( console.warn );
