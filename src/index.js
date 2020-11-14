27853c947336b747cdbd2aebb38320bb5fb66920
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 16:12:09 -0500

    [+] JavaScript: Operador Ternario

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-//  Async/Await
-const 
-    apiKey = '6t2zjzJn3x3LILIFUpF6GKKcR95JTJhK',
-    URL = `http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`;
+//  Operador Ternario
+const active = true;
+let message = '';
 
-const getImage = async () => {                  
-    
-    try {
-        const 
-            response = await fetch( URL ),          //  -> Retorna una Promesa
-            { data } = await response.json(),       //  -> Retorna una Promesa
-            { original: { url } } = data.images;
+//  Condicional if-else tradicional
+if( ! active ) {
+    message = 'Activo';
+}
+else {
+    message = 'Inactivo';
+}
 
-        console.log( url );
-        
-        const $img = document.createElement( 'img' );
+console.log( message );                     //  Inactivo
 
-        $img.src = url;
-        document.body.append( $img );
+//  Ejemplo anterior escrito con el operador ternario
+const msg = ! active ? 'Activo' : 'Inactivo';
+console.log( msg );                         //  Inactivo
 
-    } 
-    catch( error ) {
-        console.error( error );
-        //  Manejar el error con una imagen por defecto
-    }
-}    
-
-getImage();
+//  Ejemplo anterior abreviado 
+const mensajito = ! active && 'Activo';     //  Equivale a escribir un if simple
+console.log( mensajito );                   //  false
