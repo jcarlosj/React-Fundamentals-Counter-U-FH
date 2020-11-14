0bd016f7c1fba1207da378f3f0fb30c59213c20f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 15:42:16 -0500

    [+] JavaScript: Async. Crear una promesa a partir de la definicion de una funcion asyncrona

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-//  Fetch API
+//  Async
 const 
     apiKey = '6t2zjzJn3x3LILIFUpF6GKKcR95JTJhK',
     url = `http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`;
 
-// const httpRequest = fetch( url );
-
-
-const getImage = () => new Promise( ( resolve, reject ) => fetch( url ) ? resolve( fetch( url ) ) : reject( 'No hay url disponible' ) );    //  Implementa return implicito abreviado
+const getImage = async () => fetch( url ) ? fetch( url ) : 'No hay url disponible';    //  -> Retorna una Promesa > Implementa return implicito abreviado
 
 getImage()                                  //  -> Retorna una Promesa
     .then( response => response.json() )    //  -> Retorna una Promesa
