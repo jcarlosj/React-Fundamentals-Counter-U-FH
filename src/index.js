8aa33d65ae0f175c52108d750e7f891be7124d4b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 15:56:01 -0500

    [+] JavaScript: Async/Await Funcion para desplegar gif aleatoriamente

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-//  Async
+//  Async/Await
 const 
     apiKey = '6t2zjzJn3x3LILIFUpF6GKKcR95JTJhK',
-    url = `http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`;
+    URL = `http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`;
 
-const getImage = async () => fetch( url ) ? fetch( url ) : 'No hay url disponible';    //  -> Retorna una Promesa > Implementa return implicito abreviado
+const getImage = async () => {                  
+    const 
+        response = await fetch( URL ),          //  -> Retorna una Promesa
+        { data } = await response.json(),       //  -> Retorna una Promesa
+        { original: { url } } = data.images;
 
-getImage()                                  //  -> Retorna una Promesa
-    .then( response => response.json() )    //  -> Retorna una Promesa
-    .then( ({ data }) => {
-        const { original: { url } } = data.images;                //  -> Retorna los datos
+    console.log( url );
+    if( ! url ) return;
+    
+    const $img = document.createElement( 'img' );
 
-        console.log( url );
-        if( ! url ) return 'No hay URL';
-        
-        return url;
-    })
-    .then( url => {
-        const $img = document.createElement( 'img' );
+    $img.src = url;
+    document.body.append( $img );
+}    
 
-        $img.src = url;
-        document.body.append( $img );
-    })
-    .catch( console.warn );
+getImage();
