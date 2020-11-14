73f43efd85be39664d09ab67e754cae39e2092c0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-14 15:36:49 -0500

    [+] JavaScript: Funcion Asincrona usando Fetch API con promesas en cadena, despliega gif aleatoriamente

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const
     apiKey = '6t2zjzJn3x3LILIFUpF6GKKcR95JTJhK',
     url = `http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`;
 
-const httpRequest = fetch( url );
+// const httpRequest = fetch( url );
 
-httpRequest                                 //  -> Retorna una Promesa
+
+const getImage = () => new Promise( ( resolve, reject ) => fetch( url ) ? resolve( fetch( url ) ) : reject( 'No hay url disponible' ) );    //  Implementa return implicito abreviado
+
+getImage()                                  //  -> Retorna una Promesa
     .then( response => response.json() )    //  -> Retorna una Promesa
     .then( ({ data }) => {
         const { original: { url } } = data.images;                //  -> Retorna los datos
