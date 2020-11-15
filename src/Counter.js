dcc9ff8276d1c6567ef71694aa1431e987fcc8ce
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-15 17:09:42 -0500

    [+] CounterComponent: Establece estado, ejecuta accion usando hook useState (incrementa valor pulsando boton)

--- src/Counter.js
+++ src/Counter.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
 /** Functional Component */
 const Counter = ({ value }) => {
 
-    
+    const [ counter, setCounter ] = useState( 0 );
 
     const handleAdd = ( event ) => {
-        console.log( event );
+        setCounter( counter + 1 );      //  Otra Forma: setCounter( counter => counter + 1 ); 
     }
 
     return ( 
         <> 
             <h1>Counter</h1>
-            <h2>{ value }</h2>
+            <h2>{ counter }</h2>
 
             <button
                 onClick={ handleAdd }
