d70f8e056d7c01cd4871c49b26fde917f4ccf218
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-15 15:03:41 -0500

    [+] AppComponent: Retornar elementos HTML agrupados usando 'Short Fragments'

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 /** Functional Component */
 const App = () => {
-    return <h1>Hola Mundo desde React!</h1>;
+    return (
+        <> 
+            <h1>Hola Mundo desde React!</h1>
+            <p>Usamos un Fragment para agrupar elementos HTML y poder retornarlos como uno solo</p> 
+        </>
+    );
 }
  
 export default App;
