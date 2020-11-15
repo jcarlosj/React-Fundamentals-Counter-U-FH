d8c0f75c0c0d40e3b614bfaf31a93cbb097ec22f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-15 14:35:24 -0500

    [+] index.js: Despliege de elementos en el DOM usando React (Hola Mundo)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log( 'Hola Mundo desde React!' );
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const 
+    saludo = <h1>Hola Mundo desde React!</h1>,
+    divRoot = document.querySelector( '#root' );
+
+ReactDOM.render( saludo, divRoot );     //  ReactDOM: Inserta el elemento dentro del elemento referenciado en el DOM 
+                                        //            Además permite crear a partir de aqui el árbol de componentes de la aplicacion
+                                        //            permitiendo que puedan comunicarse unos con los otros
+
