46c3d264095128cbcbf679f9185a3a94b70704a2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-15 15:11:50 -0500

    [+] AppComponent: Imprimir variables

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 /** Functional Component */
 const App = () => {
+
+    const 
+        title = 'Hola Mundo desde React!',
+        message = 'Usamos un Fragment para agrupar elementos HTML y poder retornarlos como uno solo';
+
     return (
         <> 
-            <h1>Hola Mundo desde React!</h1>
-            <p>Usamos un Fragment para agrupar elementos HTML y poder retornarlos como uno solo</p> 
+            <h1>{ title }</h1>
+            <p>{ message }</p> 
         </>
     );
 }
