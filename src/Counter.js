6e143e226ccdf4e0bf233e733a69c70a012a6ea8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-15 17:08:02 -0500

    [+] CounterComponent: Crea Boton e implementa evento click

--- src/Counter.js
+++ src/Counter.js
@@ -3,10 +3,21 @@ import PropTypes from 'prop-types'
 
 /** Functional Component */
 const Counter = ({ value }) => {
+
+    
+
+    const handleAdd = ( event ) => {
+        console.log( event );
+    }
+
     return ( 
         <> 
             <h1>Counter</h1>
             <h2>{ value }</h2>
+
+            <button
+                onClick={ handleAdd }
+            >+ 1</button>
         </>
     );
 }
