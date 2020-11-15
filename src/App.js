102e5fb00f07661cc40be3d0230a50df85efba27
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-15 15:20:52 -0500

    [+] AppComponent: Imprimir objetos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 /** Functional Component */
 const App = () => {
 
-    const 
-        title = 'Hola Mundo desde React!',
-        message = 'Usamos un Fragment para agrupar elementos HTML y poder retornarlos como uno solo';
+    const data = {
+        title: 'Hola Mundo desde React!',
+        message: 'Usamos un Fragment para agrupar elementos HTML y poder retornarlos como uno solo'
+    };
 
     return (
         <> 
-            <h1>{ title }</h1>
-            <p>{ message }</p> 
+            <h1>{ data.title }</h1>
+            <p>{ data.message }</p> 
+            <pre>{ JSON.stringify( data, null, 4 ) }</pre>
         </>
     );
 }
