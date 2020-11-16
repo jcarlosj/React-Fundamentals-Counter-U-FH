062f03c408a231f0df3c4085f2c91a5702a9ffb8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-16 16:43:42 -0500

    [+] DemoTest: Define 3 pruebas estaticas

---
+++ src/tests/demo.test.js
@@ -0,0 +1,20 @@
+const can = {
+    name: 'pamplemousse',
+    ounces: 12,
+};
+  
+describe( 'the can', () => {
+
+    test( 'has 12 ounces', () => {
+      expect( can.ounces ).toBe( 12 );
+    });
+  
+    test( 'has a sophisticated name', () => {
+        expect( can.name ).toBe( 'pamplemousse' );
+    });
+
+    test( 'does not contain raspberries', () => {
+        expect( can.name ).not.toBe( 'raspberries' );
+    });
+
+});
