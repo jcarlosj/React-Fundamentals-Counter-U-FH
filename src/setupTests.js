c56c2d3a089a75083477affb54549f759e95e777
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-17 11:05:51 -0500

    [+] Test: Agrega Configuracion Global de Enzyme dependencia enzyme-to-json
    enzyme-to-json
    Convierta envoltorios de enzime a un formato compatible con las pruebas de instantáneas de Jest.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,9 @@
 
 /** Configuracion Global para Enzyme */
 import { configure } from 'enzyme';
+import { createSerializer  } from 'enzyme-to-json';
+ 
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer( createSerializer({ mode: 'deep' }) );
