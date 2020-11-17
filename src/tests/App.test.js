8479af46570eb8e87c43159c7aa0371eb8a5de4c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-17 11:29:53 -0500

    [+] AppTest (Enzyme): Define pruebas del Componente

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,30 +1,49 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
 
 import App from '../App';
 
 describe( '<App />', () => {
 
-    test( 'it should display the message "Hola, Eva Sofia Janeth! (Forma 1)', () => {
+//     test( 'it should display the message "Hola, Eva Sofia Janeth! (Forma 1)', () => {
     
-        const 
-            name = 'Eva Sofia Janeth',
-            title = 'Hola, Eva Sofia Janeth!',
-            { getByText } = render( <App name={ name } /> );        //  Monta el Componente de React & Extrae la propiedad del Componente
+//         const 
+//             name = 'Eva Sofia Janeth',
+//             title = 'Hola, Eva Sofia Janeth!',
+//             { getByText } = render( <App name={ name } /> );        //  Monta el Componente de React & Extrae la propiedad del Componente
 
-        expect( getByText( title ) ).toBeInTheDocument();           //  Usa la propiedad del Componente
+//         expect( getByText( title ) ).toBeInTheDocument();           //  Usa la propiedad del Componente
 
-    });
+//     });
 
-    test( 'it should display the message "Hola, Eva Sofia Janeth! (Forma 2)', () => {
+//     test( 'it should display the message "Hola, Eva Sofia Janeth! (Forma 2)', () => {
     
-        const 
-            name = 'Eva Sofia Janeth',
-            title = 'Hola, Eva Sofia Janeth!';
+//         const 
+//             name = 'Eva Sofia Janeth',
+//             title = 'Hola, Eva Sofia Janeth!';
             
-        render( <App name={ name } /> );    //  Monta el Componente de React
+//         render( <App name={ name } /> );    //  Monta el Componente de React
 
-        expect( screen.getByText( title ) ).toBeInTheDocument();    //  Usa screen para hacer uso de la propiedad del componente
+//         expect( screen.getByText( title ) ).toBeInTheDocument();    //  Usa screen para hacer uso de la propiedad del componente
 
-    });
+//     });
+
+    describe( 'Enzyme:', () => {
+        
+        test( 'it should display the message "Hola, Eva Sofia Janeth!', () => {
+            
+            const 
+                name = 'Eva Sofia Janeth', 
+                wrapper = shallow( <App name={ name } /> );
+
+            // console.log( wrapper.debug() );
+
+            expect( wrapper ).toMatchSnapshot();    //  Verifica que un valor coincida con la instantánea (Snapshop) más reciente
+            
+        });
+
+    }); 
 
 });
+
