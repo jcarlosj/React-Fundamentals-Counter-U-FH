d8462bc85aa6f45a66ee0b493683494e10fc3a5f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-17 10:47:40 -0500

    [+] Test: Agrega Configuracion Global de Enzyme, Remueve la de Testing Library

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,8 @@
 /** Configuracion Global para Testing Library */
-import '@testing-library/jest-dom/extend-expect';       //  Corrige -> TypeError: expect(...).toBeInTheDocument is not a function
+//  import '@testing-library/jest-dom/extend-expect';       //  Corrige -> TypeError: expect(...).toBeInTheDocument is not a function
+
+/** Configuracion Global para Enzyme */
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
