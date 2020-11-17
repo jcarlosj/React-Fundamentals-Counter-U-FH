27185f1f6c33af9ce8ec63f5ae016ac5bc6ba9da
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-17 14:41:10 -0500

    [+] CounterTest (Enzyne): Valida que los botones +1/-1 incrementen/decrementen cuando son pulsados

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -4,12 +4,12 @@ import { shallow } from 'enzyme';
 import Counter from '../Counter';
 
 describe( '<Counter />', () => {
+
+    const wrapper = shallow( <Counter /> );
     
     describe( 'Enzime:', () => {
         
         test( 'should deploy the component correctly', () => {
-           
-            const wrapper = shallow( <Counter /> );
 
             // console.log( wrapper.debug() );
 
@@ -22,7 +22,7 @@ describe( '<Counter />', () => {
             const 
                 defaultValue = 100,
                 wrapper = shallow( <Counter value={ defaultValue } /> ),
-                h2Value = wrapper.find( 'h2' ).text().trim();       //  Encuentra un elemento h2 en el Componente
+                h2Value = wrapper.find( 'h2' ).text().trim();       //  Encuentra un elemento h2 en el Componente y extrae el texto contenido
 
             // console.log( wrapper.debug() );
 
@@ -30,7 +30,42 @@ describe( '<Counter />', () => {
 
         });
         
+        describe( 'Botones', () => {
+           
+            test( 'should increase the value when clicking +1', () => {
+                
+                const
+                    currentValue = wrapper.find( 'h2' ).text().trim(), 
+                    btnAddOne =  wrapper.find( 'button' ).at( 0 );    //  Obtengo el primer boton
 
+                btnAddOne.simulate( 'click' );                          //  Simula un click sobre el boton
+
+                const finalValue = wrapper.find( 'h2' ).text().trim();     //  Encuentra un elemento h2 en el Componente y extrae el texto contenido
+
+                // console.log( btnAddOne.html(), currentValue, finalValue );
+
+                expect( ( Number( currentValue ) + 1 ).toString() ).toBe( finalValue );
+
+            });
+
+            test( 'should increase the value when clicking -1', () => {
+                
+                const
+                    currentValue = wrapper.find( 'h2' ).text().trim(), 
+                    btnSubtractOne =  wrapper.find( 'button' ).at( 2 );    //  Obtengo el tercer boton
+
+                btnSubtractOne.simulate( 'click' );                          //  Simula un click sobre el boton
+
+                const finalValue = wrapper.find( 'h2' ).text().trim();     //  Encuentra un elemento h2 en el Componente y extrae el texto contenido
+
+                // console.log( btnSubtractOne.html(), currentValue, finalValue );
+
+                expect( ( Number( currentValue ) - 1 ).toString() ).toBe( finalValue );
+
+            });
+            
+        });
+        
     });
     
 });
